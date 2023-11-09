c6af334b59b3a45d1de58858f84cc0c28ad14bed
Andrey Ivanov <aportmail2@gmail.com>
2023-11-09 17:17:27 +0300

    state-via-props

---
+++ 12-state-via-props/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({onClick}) => {
+    return (
+        <button onClick={onClick}>
+            Click me!
+        </button>
+    );
+};
+
+export default Button;
