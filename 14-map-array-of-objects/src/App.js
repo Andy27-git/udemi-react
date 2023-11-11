45bd5da6dc3ea3c3d4d9763ee30f732f24a81b02
Andrey Ivanov <aportmail2@gmail.com>
2023-11-11 16:26:16 +0300

    map array of objects

--- 14-map-array-of-objects/src/App.js
+++ 14-map-array-of-objects/src/App.js
@@ -1,15 +1,10 @@
-import './App.css';
-import persons from './components/data/persons';
-import Person from './components/Person';
+import "./App.css";
+import Persons from "./components/Persons";
 
 function App() {
-
   return (
     <div className="App">
-      <Person />  
-      {persons.map((person) => {    
-        return <h1>{person.name}</h1>
-      })}
+      <Persons />
     </div>
   );
 }
--- 14-map-array-of-objects/src/components/Person.js
+++ 14-map-array-of-objects/src/components/Person.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import persons from './data/persons';
+import React from "react";
 
-const Person = () => {
-    console.log(persons.map((person) => {
-        // person.guid, person.name
-    }));
-    return (
-        <div>
-            
-        </div>
-    );
+// const Person = ({guid, name, email, phone, company}) => {
+//     console.log(guid, name, email, phone, company);
+const Person = (props) => {
+  const { name, email, phone, company } = props;
+  return (
+    <div className="card">
+      <h3>{name}</h3>
+      <h4>{email}</h4>
+      <h4>{phone}</h4>
+      <h4>{company}</h4>
+    </div>
+  );
 };
 
 export default Person;
